9257abaf2af98fc7dbc18745cd3fcdbaa3f26d41
nico-home <caramesgarcianico@gmail.com>
2023-08-13 13:23:46 +0200

    init db y script api1

--- Script api1 + puntuaciones/api1+puntuaciones.sql
+++ Script api1 + puntuaciones/api1+puntuaciones.sql
@@ -17,9 +17,6 @@ CREATE TABLE `users` (
   UNIQUE KEY `email` (`email`)
 );
 
-
-
-
 CREATE TABLE `coments` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `userId` int unsigned DEFAULT NULL,
@@ -30,9 +27,6 @@ CREATE TABLE `coments` (
   KEY `userId` (`userId`),
   CONSTRAINT `photos_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`)
 );
-
-
-
 
 CREATE TABLE `likes` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
@@ -46,12 +40,6 @@ CREATE TABLE `likes` (
   CONSTRAINT `likes_ibfk_2` FOREIGN KEY (`fotoId`) REFERENCES `coments` (`id`)
 ) ;
 
-
-
-
-
-
-
 CREATE TABLE `puntuaciones` (
   `idpuntuaciones` int unsigned NOT NULL AUTO_INCREMENT,
   `puntuacion` int unsigned DEFAULT NULL,
@@ -63,3 +51,56 @@ CREATE TABLE `puntuaciones` (
   PRIMARY KEY (`idpuntuaciones`)
 
 ) ;
+
+            -- CREATE DATABASE  IF NOT EXISTS api1;
+            -- USE api1;
+
+            -- DROP TABLE IF EXISTS puntuacion;
+            -- DROP TABLE IF EXISTS likes;
+            -- DROP TABLE IF EXISTS coments;
+            -- DROP TABLE IF EXISTS users;
+
+            -- CREATE TABLE users (
+            -- id int unsigned NOT NULL AUTO_INCREMENT,
+            -- name varchar(50) DEFAULT NULL,
+            -- password varchar(255) DEFAULT NULL,
+            -- email varchar(100) DEFAULT NULL,
+            -- dia_registro DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+            -- active tinyint(1) DEFAULT NULL,
+            -- PRIMARY KEY (id),
+            -- UNIQUE KEY email (email)
+            -- );
+
+            -- CREATE TABLE coments (
+            -- id int unsigned NOT NULL AUTO_INCREMENT,
+            -- userId int unsigned DEFAULT NULL,
+            -- image varchar(255) DEFAULT NULL,
+            -- text varchar(255) DEFAULT NULL,
+            -- create_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+            -- PRIMARY KEY (id),
+            -- KEY userId (userId),
+            -- CONSTRAINT photos_ibfk_1 FOREIGN KEY (userId) REFERENCES users (id)
+            -- );
+
+            -- CREATE TABLE likes (
+            -- id int unsigned NOT NULL AUTO_INCREMENT,
+            -- userId int unsigned DEFAULT NULL,
+            -- fotoId int unsigned DEFAULT NULL,
+            -- dia_creacion datetime DEFAULT NULL,
+            -- PRIMARY KEY (id),
+            -- KEY userId (userId),
+            -- KEY fotoId (fotoId),
+            -- CONSTRAINT likes_ibfk_1 FOREIGN KEY (userId) REFERENCES users (id),
+            -- CONSTRAINT likes_ibfk_2 FOREIGN KEY (fotoId) REFERENCES coments (id)
+            -- ) ;
+
+            -- CREATE TABLE puntuaciones (
+            -- idpuntuaciones int unsigned NOT NULL AUTO_INCREMENT,
+            -- puntuacion int unsigned DEFAULT NULL,
+            -- intentos int unsigned DEFAULT NULL,
+            -- nombre varchar(50) DEFAULT NULL,
+            -- ultima tinyint(1) DEFAULT NULL,
+            -- tiempo varchar(255) DEFAULT NULL,
+            -- create_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+            -- PRIMARY KEY (idpuntuaciones)
+            -- ) ;
---
+++ db/initDb.js
@@ -0,0 +1,78 @@
+require("dotenv").config();
+
+const getDB = require("./getDb");
+
+async function main() {
+    let conexion;
+    try {
+        conexion = await getDB();
+
+        console.log("creando database si no existe api1");
+        await conexion.query("CREATE DATABASE  IF NOT EXISTS api1;");
+        await conexion.query("USE api1;");
+
+        console.log("borrado tablas");
+        await conexion.query("DROP TABLE IF EXISTS puntuacion;");
+        await conexion.query("DROP TABLE IF EXISTS likes;");
+        await conexion.query("DROP TABLE IF EXISTS coments;");
+        await conexion.query("DROP TABLE IF EXISTS users;");
+
+        console.log("creando tablas");
+        await conexion.query(`
+            CREATE TABLE users (
+            id int unsigned NOT NULL AUTO_INCREMENT,
+            name varchar(50) DEFAULT NULL,
+            password varchar(255) DEFAULT NULL,
+            email varchar(100) DEFAULT NULL,
+            dia_registro DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+            active tinyint(1) DEFAULT NULL,
+            PRIMARY KEY (id),
+            UNIQUE KEY email (email)
+            );
+        `);
+        await conexion.query(`
+        CREATE TABLE coments (
+            id int unsigned NOT NULL AUTO_INCREMENT,
+            userId int unsigned DEFAULT NULL,
+            image varchar(255) DEFAULT NULL,
+            text varchar(255) DEFAULT NULL,
+            create_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+            PRIMARY KEY (id),
+            KEY userId (userId),
+            CONSTRAINT photos_ibfk_1 FOREIGN KEY (userId) REFERENCES users (id)
+            );
+    `);
+        await conexion.query(`
+    CREATE TABLE likes (
+        id int unsigned NOT NULL AUTO_INCREMENT,
+        userId int unsigned DEFAULT NULL,
+        fotoId int unsigned DEFAULT NULL,
+        dia_creacion datetime DEFAULT NULL,
+        PRIMARY KEY (id),
+        KEY userId (userId),
+        KEY fotoId (fotoId),
+        CONSTRAINT likes_ibfk_1 FOREIGN KEY (userId) REFERENCES users (id),
+        CONSTRAINT likes_ibfk_2 FOREIGN KEY (fotoId) REFERENCES coments (id)
+        ) ;
+`);
+        await conexion.query(`
+CREATE TABLE puntuaciones (
+    idpuntuaciones int unsigned NOT NULL AUTO_INCREMENT,
+    puntuacion int unsigned DEFAULT NULL,
+    intentos int unsigned DEFAULT NULL,
+    nombre varchar(50) DEFAULT NULL,
+    ultima tinyint(1) DEFAULT NULL,
+    tiempo varchar(255) DEFAULT NULL,
+    create_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (idpuntuaciones)
+    ) ;
+`);
+    } catch (error) {
+        console.log(error);
+    } finally {
+        if (conexion) conexion.release();
+        process.exit();
+    }
+}
+
+main();
